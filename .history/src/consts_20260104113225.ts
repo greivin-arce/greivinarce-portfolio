773cb0a691ab885673c0b2a6afc3b838bc1be977
Greivin122 <greivin.arce.n@icloud.com>
2026-01-05 11:16:52 -0600

    feat: remove redundant home link from header

--- .history/src/consts_20260104113225.ts
+++ .history/src/consts_20260104113225.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Greivin Arce";
+export const SITE_DESCRIPTION = "Backend Software Engineer focused on secure, scalable systems. Experienced in Java, Spring Boot, REST APIs, and enterprise integrations.";
